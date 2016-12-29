53bb96cff794e3e27b89245295ffbcdccf6bb77b
rui.liu <rui.liu@17zuoye.com>
2016-12-29 19:26:45 +0800

    将Ares-PixiJS独立成一个项目

--- dist/ares_pixi.js
+++ dist/ares_pixi.js
@@ -330,7 +330,7 @@ var ares;
             };
             PIXICompiler._textExpReg = /(.*?)\{\{(.*?)\}\}(.*)/;
             return PIXICompiler;
-        })();
+        }());
         pixijs.PIXICompiler = PIXICompiler;
     })(pixijs = ares.pixijs || (ares.pixijs = {}));
 })(ares || (ares = {}));
